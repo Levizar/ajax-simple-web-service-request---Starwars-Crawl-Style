90742810791f579b8caa47c35ea083068e8c26bc
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-03 21:46:44 +0100

    fixed scss prop that make the text appear in the middle

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,12 @@
             const quoteReceived = await quoteRequest.json();
             console.log(quoteReceived);
             const author   = await quoteReceived.author;
-            const quote    = await quoteReceived.quote;
+            const quote    = await `"${quoteReceived.quote}"`;
             const photoSrc = await quoteReceived.photo;
+
+            if (photoSrc == undefined || photoSrc == null || photoSrc == ""){
+                document.querySelector(".crawl img").remove();
+            }
 
             document.getElementById("author").innerHTML = author;
             document.getElementById("quote").innerHTML = quote;
