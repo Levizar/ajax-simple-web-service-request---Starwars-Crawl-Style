ef4ea0a100844be4d5ad2c8b1e41cf8194d722be
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-03 18:16:31 +0100

    Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 
+# AJAX request Quote #
+
+This ripository contain an exercise realised during my internship at Becode on sunday the 3th of November.
+The point of this project was to make an AJAX request to get a quote from an extern API.
+
+I've reused The Background I've made on my [404 project](https://levizar.github.io/404/) and the crawling of [my Star Wars style Crawl project](https://levizar.github.io/Star-Wars-Crawl/).
+
+[Here is the link to the page](https://levizar.github.io/ajax-simple-web-service-request/).
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@
     window.addEventListener("load", async () => {
         console.log('yosh load');
         try {
-            const quoteRequest   = await fetch("https://thatsthespir.it/api");
+            const quoteRequest  = await fetch("https://thatsthespir.it/api");
             const quoteReceived = await quoteRequest.json();
             console.log(quoteReceived);
-            const author = quoteReceived.author;
-            const quote = quoteReceived.quote;
-            const photoSrc = quoteReceived.photo;
+            const author   = await quoteReceived.author;
+            const quote    = await quoteReceived.quote;
+            const photoSrc = await quoteReceived.photo;
 
             document.getElementById("author").innerHTML = author;
             document.getElementById("quote").innerHTML = quote;
